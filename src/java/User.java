8d379e65f9577968d95b1104a8a9177c24e268be
Niall Quinn <niall@niallquinn.me>
2014-11-22 14:28:30 +0000

    initial commit

---
+++ src/java/User.java
@@ -0,0 +1,31 @@
+/*
+ * Copyright 2014 niallquinn
+ * User.java created Nov 22, 2014
+ * WebDevProject
+ */
+/**
+ *
+ * @author niallquinn
+ */
+public class User
+{
+    private String firstName;
+    private String lastName;
+    private String dob;
+    
+    public String getFirstName() {
+        return firstName;
+    }
+    
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+    
+    public String getLastName() {
+        return lastName;
+    }
+    
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+}
